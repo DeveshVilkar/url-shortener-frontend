f1c63e5bd78a30b340ee3f8f283146a27d95a90f
Devesh Vilkar <deveshvilkar@Deveshs-MacBook-Air.local>
2025-06-27 00:34:29 +0530

    Configured environment for production and updated API URL

--- src/app/services/url-service.ts
+++ src/app/services/url-service.ts
@@ -1,18 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UrlService {
 
-  private URL="http://localhost:3000/api"
+  constructor(private http: HttpClient) { }
 
-  constructor(private http:HttpClient) { }
-
-  getShortenedUrl(url:string):Observable<any>{
-    return this.http.post(`${this.URL}/shorturl`,{longUrl:url})
+  getShortenedUrl(url: string): Observable<any> {
+    return this.http.post(`${environment.apiUrl}/shorturl`, { longUrl: url })
   }
 
 
