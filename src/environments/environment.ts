b748f68eb1dc8c35be353935c543283573ab8411
Devesh Vilkar <deveshvilkar@Deveshs-MacBook-Air.local>
2025-06-28 11:51:25 +0530

    render env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://url-shortener-backend-zeta-one.vercel.app/api'
+  apiUrl: 'https://url-shortener-backend-6q03.onrender.com/api'
 };
